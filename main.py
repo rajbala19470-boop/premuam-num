e2d1c0dc73e9356e0a4a1ef03956b232f4ac4159
rajbala19470-boop <rajbala19470@gmail.com>
2026-07-27 13:32:26 +0600

    Reduce OTP poll interval from 4 to 1 second

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ ADMIN_IDS = [8744359777]
 OTP_GROUP_URL = "https://t.me/RgxOtp"
 OTP_API_URL = "http://127.0.0.1:5080/all_otp"
 OTP_API_TOKEN = "e84466454aeadf8b442cc602d2b265d4"
-OTP_POLL_INTERVAL = 4  # seconds
+OTP_POLL_INTERVAL = 1  # seconds
 
 MIN_WITHDRAW = 0.1  # USD
 
